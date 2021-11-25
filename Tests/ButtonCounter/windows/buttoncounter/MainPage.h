253cae4d60ed6686c851375e68af740883c23b82
Ryan <rcbarr@ilstu.edu>
2021-11-24 20:33:08 -0800

    initial commit

---
+++ Tests/ButtonCounter/windows/buttoncounter/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::buttoncounter::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::buttoncounter::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
